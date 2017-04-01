f888bf6c3bf93ca64b63e657676fc2f8675913e0
天呐 <290818743@qq.com>
2017-04-01 10:59:51 +0800

    Update 写入日志.php

--- 写入日志.php
+++ 写入日志.php
@@ -10,3 +10,22 @@
         flock($fp, LOCK_UN);
         fclose($fp);
     }
+
+/**
+ * 分页
+ * $count_total总条数
+ * $page_sum当前条数
+ * $topicList需要分页的数组
+ * $page_number当前页码数
+ * 
+ */
+ function pagePage($page_sum,$page_number,$topicList)
+ {
+     $count_total = count($topicList);
+     $merge_page = intval(floor($count_total / $page_sum)) + 1;//总页数
+     $newarr['list'] = array_slice($topicList, ($page_number-1)*$page_sum, $page_sum);
+     $merge['page'] = $merge_page;
+     //合并数组
+     $result = array_merge_recursive($newarr,$merge);
+     return $result;
+ }
